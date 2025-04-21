cf76c5717e032af70f9b64ffa103adf3568d3999
kly777 <kly777777777@gmail.com>
2025-04-21 20:36:00 +0800

    refactor(wxt.config.ts): 优化 terserOptions 配置
    - 在生产模式下启用 drop_console 和 drop_debugger 选项
    - 在非生产模式下禁用这些选项

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,26 +2,28 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-    modules: ["@wxt-dev/module-vue"],
-    browser: "chrome",
-    manifest: {
-        permissions: ["storage"],
-    },
-    zip: {
-        downloadPackages: [
-            "@kly777/bread",
-            //...
-        ],
-    },
-    vite: () => ({
-        build: {
-            minify: "terser",
-            terserOptions: {
-                compress: {
-                    drop_console: true,
-                    drop_debugger: true,
-                },
-            },
-        },
-    }),
+	modules: ["@wxt-dev/module-vue"],
+	browser: "chrome",
+	manifest: {
+		permissions: ["storage"],
+	},
+	zip: {
+		downloadPackages: [
+			"@kly777/bread",
+			//...
+		],
+	},
+	vite: (configEnv) => ({
+		build: {
+			minify: "terser",
+			terserOptions: {
+				compress: configEnv.mode === "production"
+					? {
+						drop_console: true,
+						drop_debugger: true,
+					}
+					: false,
+			},
+		},
+	}),
 });
