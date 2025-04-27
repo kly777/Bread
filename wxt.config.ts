8ab5b6c8d5cbe6670302a27a870b45442c524144
kly777 <kly777777777@gmail.com>
2025-04-27 14:00:56 +0800

    style(wxt.config): 格式化 wxt 配置文件
    - 使用了更一致的缩进方式（推荐使用 4 个空格）

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,28 +2,29 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-	modules: ["@wxt-dev/module-vue"],
-	browser: "chrome",
-	manifest: {
-		permissions: ["storage"],
-	},
-	zip: {
-		downloadPackages: [
-			"@kly777/bread",
-			//...
-		],
-	},
-	vite: (configEnv) => ({
-		build: {
-			minify: "terser",
-			terserOptions: {
-				compress: configEnv.mode === "production"
-					? {
-						drop_console: true,
-						drop_debugger: true,
-					}
-					: false,
-			},
-		},
-	}),
+    modules: ["@wxt-dev/module-vue"],
+    browser: "chrome",
+    manifest: {
+        permissions: ["storage"],
+    },
+    zip: {
+        downloadPackages: [
+            "@kly777/bread",
+            //...
+        ],
+    },
+    vite: (configEnv) => ({
+        build: {
+            minify: "terser",
+            terserOptions: {
+                compress:
+                    configEnv.mode === "production"
+                        ? {
+                              drop_console: true,
+                              drop_debugger: true,
+                          }
+                        : false,
+            },
+        },
+    }),
 });
