afefd30683e70dbf51a9910c993ac5dd51c8c906
kly777 <kly777777777@gmail.com>
2025-05-09 23:09:49 +0800

    refactor(wxt.config): 优化代码格式和结构
    - 调整代码缩进和换行，提高可读性
    - 移除冗余的空行，使代码更加紧凑

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,36 +1,36 @@
 // See https://wxt.dev/api/config.html
 import { defineConfig } from "wxt";
 export default defineConfig({
-    modules: ["@wxt-dev/module-vue"],
-    browser: "chrome",
-    manifest: {
-        permissions: [
-            "storage",
-            "https://edge.microsoft.com/",
-            "https://api-edge.cognitive.microsofttranslator.com/",
-            "https://translate.googleapis.com/translate_a/single",
-        ],
-        summary: "A browser extension for reading enhancement",
+  modules: ["@wxt-dev/module-vue"],
+  browser: "chrome",
+  manifest: {
+    permissions: [
+      "storage",
+      "https://edge.microsoft.com/",
+      "https://api-edge.cognitive.microsofttranslator.com/",
+      "https://translate.googleapis.com/translate_a/single",
+    ],
+    summary: "A browser extension for reading enhancement",
+  },
+  zip: {
+    downloadPackages: [
+      "@kly777/bread",
+      //...
+    ],
+  },
+  vite: (configEnv: { mode: string }) => ({
+    build: {
+      // 根据模式切换 minify 行为
+      minify: configEnv.mode === "production" ? "terser" : false,
+      terserOptions: {
+        compress:
+          configEnv.mode === "production"
+            ? {
+              drop_console: true,
+              drop_debugger: true,
+            }
+            : false,
+      },
     },
-    zip: {
-        downloadPackages: [
-            "@kly777/bread",
-            //...
-        ],
-    },
-    vite: (configEnv: { mode: string }) => ({
-        build: {
-            // 根据模式切换 minify 行为
-            minify: configEnv.mode === "production" ? "terser" : false,
-            terserOptions: {
-                compress:
-                    configEnv.mode === "production"
-                        ? {
-                              drop_console: true,
-                              drop_debugger: true,
-                          }
-                        : false,
-            },
-        },
-    }),
+  }),
 });
