1fe3c2520f028f372b84394a393f2772309148a0
口x口 <kly777777777@gmail.com>
2025-10-30 23:09:43 +0800

    fmt

--- wxt.config.ts
+++ wxt.config.ts
@@ -21,8 +21,8 @@ export default defineConfig({
                                 //         "telemetry": false,
                                 //         "study": false
                                 // }
-                        }
-                }
+                        },
+                },
         },
         zip: {
                 downloadPackages: [
