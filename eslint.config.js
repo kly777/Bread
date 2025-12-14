6bf22285d285c306b7c6e7ee80e2791540bcc78f
口x口 <kly777777777@gmail.com>
2025-12-14 23:13:20 +0800

    del vue in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'eslint/config'
 import js from '@eslint/js'
 import ts from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
-import vue from 'eslint-plugin-vue'
-import vueParser from 'vue-eslint-parser'
 
 export default defineConfig([
         // 全局配置
@@ -28,7 +26,6 @@ export default defineConfig([
                 plugins: {
                         js,
                         '@typescript-eslint': ts,
-                        vue,
                 },
                 rules: {
                         'no-unused-vars': 'warn',
@@ -43,7 +40,6 @@ export default defineConfig([
 
                         ...ts.configs['recommended'].rules,
                         ...ts.configs['eslint-recommended'].rules,
-                        ...vue.configs['flat/recommended'].rules,
                 },
         },
         // TypeScript配置
@@ -53,18 +49,6 @@ export default defineConfig([
                         parser: tsParser,
                         parserOptions: {
                                 project: './tsconfig.json',
-                        },
-                },
-        },
-        // Vue配置
-        {
-                files: ['**/*.vue'],
-                languageOptions: {
-                        parser: vueParser,
-                        parserOptions: {
-                                parser: '@typescript-eslint/parser',
-                                extraFileExtensions: ['.vue'],
-                                project: './tsconfig.eslint.json',
                         },
                 },
         },
